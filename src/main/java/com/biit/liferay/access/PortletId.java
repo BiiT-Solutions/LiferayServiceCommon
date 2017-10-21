80759213b946f440c296aef297c1d65ea31aac44
Jorge Hortelano <jorgehortelano@gmail.com>
2017-10-21 11:01:44 +0200

    Added portlet id for AdminPortlet

--- src/main/java/com/biit/liferay/access/PortletId.java
+++ src/main/java/com/biit/liferay/access/PortletId.java
@@ -1,7 +1,10 @@
 package com.biit.liferay.access;
 
 public enum PortletId {
+	
 	DOCUMENT_AND_MEDIA("20"),
+	
+	ADMIN_PORTLET("com.liferay.knowledgebase.admin.portlet.AdminPortlet"),
 
 	KNOWLEDGEBASE_PORTLET("3_WAR_knowledgebaseportlet");
 
